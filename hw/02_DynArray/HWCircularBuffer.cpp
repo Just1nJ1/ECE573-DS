4bc78495550ea9580b6de19fdeb65848237de159
Haochen Ji <462499571@qq.com>
2024-02-06 14:49:16 -0500

    HW2

--- hw/02_DynArray/HWCircularBuffer.cpp
+++ hw/02_DynArray/HWCircularBuffer.cpp
@@ -6,19 +6,113 @@
 
 */
 
+#include <iostream>
+
+using namespace std;
+
 class CircularBuffer {
 private:
     int* data;
     int capacity;
     int head;
     int tail;
-public:
-    CircularBuffer(int capacity) : data(new int[capacity]), capacity(capacity), head(0), tail(0)
+    void checkGrow() {
+        if ((head + capacity) % capacity == (tail + capacity) % capacity) {
+            int* old = data;
+            data = new int[capacity * 2];
+            for (int i = head; i < tail; ++i)
+                data[(i + capacity * 2) % (capacity * 2)] = old[(i + capacity) % capacity];
 
+            capacity *= 2;
+            delete [] old;
+        }
+    }
+public:
+    explicit CircularBuffer(int capacity) : data(new int[capacity]), capacity(capacity), head(0), tail(0) {}
+    ~CircularBuffer() { delete [] data; }
+    CircularBuffer(const CircularBuffer& orig) : data(new int[orig.capacity]), capacity(orig.capacity), head(orig.head), tail(orig.tail) {
+        for (int i = head; i != tail; ++i)
+            data[(i + capacity) % capacity] = orig.data[(i + capacity) % capacity];
+    }
+
+    void addEnd(int v) {
+        checkGrow();
+        data[tail++] = v;
+    }
+
+    void addStart(int v) {
+        checkGrow();
+        data[(--head + capacity) % capacity] = v;
+    }
+
+    void removeStart() {
+        head++;
+    }
+
+    void removeEnd() {
+        tail--;
+    }
+
+    uint32_t getSize() const {
+        return tail - head;
+    }
+
+    void removeEvens() {
+        int count = head;
+        for (int i = head; i < tail; ++i)
+            if (data[(i + capacity) % capacity] % 2 == 1)
+                data[(count++ + capacity) % capacity] = data[(i + capacity) % capacity];
+        tail = count;
+    }
+
+    friend ostream& operator<<(ostream& out, const CircularBuffer& buffer) {
+        out << "Size: " << buffer.tail - buffer.head << ", Capacity: " << buffer.capacity << endl;
+#ifdef DEBUG
+        out << "Head: " << buffer.head << ", Tail: " << buffer.tail << endl;
+#endif
+        out << "Head to Tail: \t";
+        for (int i = buffer.head; i < buffer.tail; ++i)
+            out << buffer.data[(i + buffer.capacity) % buffer.capacity] << " ";
+        out << endl;
+
+        out << "In memory: \t\t";
+        for (int i = 0; i < buffer.capacity; ++i)
+            out << buffer.data[i] << " ";
+        out << endl;
+        return out;
+    }
 };
 
+int main() {
+    CircularBuffer a(5);
+    const int n = 10;
+    cout << "=====Adding " << n << " numbers into a by addEnd=====" << endl;
+    for (int i = 0; i < n; i++)
+        a.addEnd(i);
+    cout << a << '\n';
+
+    cout << "=====Adding " << n << " numbers into a by addStart=====" << endl;
+    for (int i = 0; i < n; i++)
+        a.addStart(i);
+    cout << a << '\n';
+
+    cout << "=====Adding 5 into a by addStart=====" << endl;
+    a.addStart(5);
+    cout << a << '\n';
 
 
-vector<int>
-list<int> 
+    cout << "=====Removing " << n/2 << " numbers from a by removeEnd=====" << endl;
+    for (int i = 0; i < n/2; i++)
+        a.removeEnd();
+    cout << a << '\n';
 
+    cout << "=====Removing " << n/2 << " numbers from a by removeStart=====" << endl;
+    for (int i = 0; i < n/2; i++)
+        a.removeStart();
+    cout << a << '\n';
+
+    cout << "=====Removing even numbers from a by removeEvens=====" << endl;
+    a.removeEvens();
+
+    cout << a << '\n';
+}
--- hw/02_DynArray/HWDynArray.cc
+++ hw/02_DynArray/HWDynArray.cc
@@ -1,60 +1,123 @@
+/**
+ * Authors: Haochen Ji, Rui Zhang
+ */
+
+#include <iostream>
+
+using namespace std;
+
 class GrowArray {
 private:
-	int* p;
-	uint32_t size; // the number of elements used
-	uint32_t capacity; // the amount of memory
-	void checkGrow() {
-	  if (memory does not need to grow)
-	    return;
-	  
-		// if the memory needs to grow, double in size
-		//TODO: YOU IMPLEMENT THIS
-	}
+    int* p;
+    uint32_t size; // the number of elements used
+    uint32_t capacity; // the amount of memory
+    void checkGrow() {
+        if (size < capacity) {
+            return;
+        }
+
+        int* old = p;
+        p = new int[capacity * 2];
+        for (int i = 0; i < capacity; ++i) {
+            p[i] = old[i];
+        }
+
+        capacity *= 2;
+    }
 public:
-	//write constructor, destructor
+    //write constructor, destructor
+    explicit GrowArray(unsigned int size) : p(new int[size]), size(0), capacity(size) {
+        if (capacity > 10000)
+            cerr << "Warning: Size > 10000! Potentially negative size value" << endl;
+    }
+    ~GrowArray() { delete [] p; }
 
-	//copy constructor + 10%
-	//double ended?  + 20% (SEE circularbuffer)
-	void addEnd(int v) { //O(____________)
-		
-		
-	}
+    //copy constructor + 10%
+    GrowArray(const GrowArray& orig) : p(new int[orig.capacity]), size(orig.size), capacity(orig.capacity) {
+        for (int i = 0; i < size; ++i)
+            p[i] = orig.p[i];
+    }
 
-	void addStart(int v) { //O(____________)
+    //double ended?  + 20% (SEE circularbuffer)
+    void addEnd(int v) { //O(n)
+        checkGrow();
+        p[size++] = v;
+    }
 
-	}
-	void removeStart() { //O(____________)
+    void addStart(int v) { //O(n)
+        checkGrow();
+        for (int i = (int)size; i > 0; --i) {
+            p[i] = p[i - 1];
+        }
+        p[0] = v;
+        size++;
+    }
+    void removeStart() { //O(n)
+        for (int i = 0; i < size - 1; ++i)
+            p[i] = p[i + 1];
+        size--;
+    }
 
-	}
-	
-	void removeEnd() { //O(____________)
+    void removeEnd() { //O(1)
+        p[--size] = 0;
+    }
 
-	}
-	uint32_t getSize() const { //O(____________)
-		return size;
-	}
-	
+    uint32_t getSize() const { //O(1)
+        return size;
+    }
+
+    void removeEvens() { //O(n)
+        int count = 0;
+        for (int i = 0; i < size; ++i)
+            if (p[i] % 2 == 1)
+                p[count++] = p[i];
+        size = count;
+    }
+
+    friend ostream& operator<<(ostream& out, const GrowArray& array) {
+        out << "Size: " << array.size << ", Capacity: " << array.capacity << endl;
+        for (int i = 0; i < array.size; ++i)
+            out << array.p[i] << (i == array.size - 1 ? "" : " ");
+        return out;
+    }
+
+    GrowArray& operator=(const GrowArray& other) {
+        if (this != &other) {
+            delete[] p;
+            size = other.size;
+            capacity = other.capacity;
+            p = new int[capacity];
+            for (int i = 0; i < size; ++i)
+                p[i] = other.p[i];
+        }
+        return *this;
+    }
 };
 
 int main() {
-	GrowArray a(5); // empty list, with 500 elements
-	const int n = 10;
-	for (int i = 0; i < n; i++)   //O(____________)
-		a.addEnd(i); // really fast!
+    GrowArray a(5); // empty list, with 5 elements
+    const int n = 10;
+    for (int i = 0; i < n; i++)   //O(n)
+        a.addEnd(i); // really fast!
+    cout << a << '\n';
 
-	for (int i = 0; i < n; i++)    //O(____________)
-		a.addEnd(i); // every time you need to grow, double
+    for (int i = 0; i < n; i++)    //O(n)
+        a.addEnd(i); // every time you need to grow, double
+    cout << a << '\n';
 
-	a.addStart(5);
-	
-	for (int i = 0; i < n/2; i++) //O(____________)
-		a.removeEnd();
+    a.addStart(5);
+    cout << a << '\n';
 
-	for (int i = 0; i < n/2; i++)
-		a.removeStart();
+    for (int i = 0; i < n/2; i++) //O(n)
+        a.removeEnd();
+    cout << a << '\n';
 
-	
+    for (int i = 0; i < n/2; i++)
+        a.removeStart();
+    cout << a << '\n';
+
+
     a.removeEvens();
-	
-	cout << a << '\n';
+
+    cout << a << '\n';
 }
