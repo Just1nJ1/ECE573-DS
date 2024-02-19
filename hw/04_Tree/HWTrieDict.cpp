1c1bdfa32a6bbdc2948fecf7dbba6d776525708f
Haochen Ji <462499571@qq.com>
2024-02-19 14:55:10 -0500

    HW4 Update

--- hw/04_Tree/HWTrieDict.cpp
+++ hw/04_Tree/HWTrieDict.cpp
@@ -2,38 +2,89 @@
 #include <fstream>
 using namespace std;
 
-/*
-	dict.txt is located in the data directory of the repo
-	load all the words into the trie
-
-	
-
-
- */
-class Trie {
+class TrieNode {
 public:
-	Trie(const char dict[]) {
+    bool isEndOfWord;
+    TrieNode* children[26]{};
 
-	}
-	bool is_word(const char word[]) {
-		return true;
-	}
-	bool is_prefix(const char word[]) {
-		return true;
-	}
+    TrieNode() : isEndOfWord(false) {}
+    ~TrieNode() {
+        for (auto t: children)
+            delete t;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+    void insert(const char* word) {
+        TrieNode* node = root;
+        for (int i = 0; word[i] != '\0'; ++i) {
+            int index = word[i] - 'a';
+            if (!node->children[index])
+                node->children[index] = new TrieNode();
+            node = node->children[index];
+        }
+        node->isEndOfWord = true;
+    }
+public:
+    explicit Trie(const char dict[]) {
+        root = new TrieNode();
+        char word[256];
+        ifstream f(dict);
+        while (f.getline(word, sizeof(word))) {
+            if (strncmp(word, "cat", 3) == 0) {
+                cout << "";
+            }
+            insert(word);
+        }
+    }
+
+    ~Trie() {
+        delete root;
+    }
+
+    bool is_word(const char* word) {
+        TrieNode* node = root;
+        for (int i = 0; word[i] != '\0'; ++i) {
+            int index = word[i] - 'a';
+            if (!node->children[index])
+                return false;
+            node = node->children[index];
+        }
+        return node != nullptr && node->isEndOfWord;
+    }
+
+    bool is_prefix(const char* prefix) {
+        TrieNode* node = root;
+        for (int i = 0; prefix[i] != '\0'; ++i) {
+            int index = prefix[i] - 'a';
+            if (!node->children[index])
+                return false;
+            node = node->children[index];
+        }
+        return true;
+    }
 };
 
 int main() {
-	Trie t("dict.txt"); // load dict.txt from the current directory
+	Trie t("../data/en213k.txt"); // load dict.txt from the current directory
 	
 	ifstream f("triespellcheck.txt");
 	char word[256];
 	int count = 0;
 	while (f.getline(word, sizeof(word))) {
+        size_t length = strlen(word);
+        if (length > 0 && word[length - 1] == '\r')
+            word[length - 1] = '\0';
+
 		if (!t.is_word(word))
 			cout << word << '\n';
 		if (t.is_prefix(word))
 			cout << "startswith: " << word << '\n';
 	}
+
+    cout << flush;
 	return 0;
 }
